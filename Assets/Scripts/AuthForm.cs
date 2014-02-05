a3c136445358d385d0aeafb56c37352d3c80dca8
German Viscuso <germanviscuso@gmail.com>
2014-02-05 01:49:25 -0800

    Initial commit

---
+++ Assets/Scripts/AuthForm.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using KiiCorp.Cloud.Storage;
+using System;
+
+public class AuthForm : MonoBehaviour {
+
+	bool OnCallback = false;
+	string username = "";
+	string password = "";
+	KiiUser user = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void OnGUI () {
+		if (OnCallback)
+			GUI.enabled = false;
+		else
+			GUI.enabled = true;
+		
+		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
+		GUILayout.FlexibleSpace ();
+		GUILayout.BeginHorizontal ();
+		GUILayout.FlexibleSpace ();
+		GUILayout.BeginVertical ();
+		
+		GUILayout.Label ("Username");
+		username = GUILayout.TextField (username, GUILayout.MinWidth (200));
+		GUILayout.Space (10);
+		GUILayout.Label ("Password");
+		password = GUILayout.PasswordField (password, '*', GUILayout.MinWidth (100));
+		GUILayout.Space (30);
+		
+		if (GUILayout.Button ("Login", GUILayout.MinHeight (50), GUILayout.MinWidth (100))) {
+			if( username.Length == 0 || password.Length == 0 )
+				Debug.Log ("Username/password can't be empty");
+			else {
+				Login ();
+			}
+		}
+		
+		if (GUILayout.Button ("Register", GUILayout.MinHeight (50), GUILayout.MinWidth (100))) {
+			if( username.Length == 0 || password.Length == 0 )
+				Debug.Log ("Username/password can't be empty");
+			else {
+				Register ();
+			}
+		}
+		
+		if (user != null) {
+			OnCallback = false;
+			Application.LoadLevel ("Level");
+		}
+
+		GUILayout.EndVertical ();
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndHorizontal ();
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndArea ();
+	}
+
+	private void Login () {
+		user = null;
+		OnCallback = true;
+		KiiUser.LogIn(username, password, (KiiUser user2, Exception e) => {
+			if (e == null) {
+				Debug.Log ("Login completed");
+				user = user2;
+			} else {
+				user = null;
+				OnCallback = false;
+				Debug.Log ("Login failed : " + e.ToString());
+			}
+		});
+	}
+	
+	private void Register () {
+		user = null;
+		OnCallback = true;
+		KiiUser built_user = KiiUser.BuilderWithName (username).Build ();
+		built_user.Register(password, (KiiUser user2, Exception e) => {
+			if (e == null)
+			{
+				user = user2;
+				Debug.Log ("Register completed");
+			} else {
+				user = null;
+				OnCallback = false;
+				Debug.Log ("Register failed : " + e.ToString());
+			}
+			
+		});
+	}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using KiiCorp.Cloud.Storage;
+using System;
+
+public class Score : MonoBehaviour
+{
+	public int score = 0;					// The player's score.
+	public static int highscore = 0;
+
+	private PlayerControl playerControl;	// Reference to the player control script.
+	private int previousScore = 0;			// The score in the previous frame.
+
+
+	void Awake ()
+	{
+		// Setting up the reference.
+		playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+	}
+
+
+	void Update ()
+	{
+		// Set the score and user text.
+		if (KiiUser.CurrentUser != null)
+		{
+			if(highscore == 0)
+				LoadHighScore ();
+			string username = KiiUser.CurrentUser.Username;
+			guiText.text = "Score: " + score + "  Highscore: " + highscore + "  User: " + username;
+		}
+		else
+			guiText.text = "Score: " + score;
+
+		// If the score has changed...
+		if(previousScore != score){
+			// ... play a taunt.
+			playerControl.StartCoroutine(playerControl.Taunt());
+			SaveScore (score);
+			LoadHighScore ();
+		}
+
+		// Set the previous score to this frame's score.
+		previousScore = score;
+	}
+
+	void SaveScore (int score)
+	{
+		KiiUser user = KiiUser.CurrentUser;
+		if (user == null) {
+			return;
+		}
+		KiiBucket bucket = user.Bucket ("scores");
+		KiiObject kiiObj = bucket.NewKiiObject ();
+		kiiObj ["score"] = score;
+		kiiObj ["time"] = Time.time;
+		kiiObj ["delta_time"] = Time.deltaTime;
+		
+		kiiObj.Save((KiiObject obj, Exception e) => {
+			if (e != null)
+				Debug.LogError(e.ToString());
+			else
+				Debug.Log("Score sent: " + score.ToString());
+		});
+	}
+
+	public static void LoadHighScore () {
+		if (KiiUser.CurrentUser == null) {
+			return;
+		}
+		//if (highscore > 0) {
+		//	return highscore;
+		//}
+		
+		KiiUser user = KiiUser.CurrentUser;
+		KiiBucket bucket = user.Bucket ("scores");
+		KiiQuery query = new KiiQuery ();
+		query.SortByDesc ("score");
+		query.Limit = 1;
+
+		bucket.Query(query, (KiiQueryResult<KiiObject> list, Exception e) =>{
+			if (e != null)
+			{
+				Debug.Log ("Failed to load high score " + e.ToString());
+			} else {
+				foreach (KiiObject obj in list) {
+					highscore = obj.GetInt ("score", 0);
+					Debug.Log ("High score loaded: " + highscore.ToString());
+					return;
+				}
+			}
+		});
+
+	}
+}
